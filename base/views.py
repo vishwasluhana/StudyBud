f162df826e42a9156dc89133f727b4e943ccb42e
Vishwa Mittar <vishwasluhana@gmail.com>
2022-09-21 01:51:17 +0500

    create Topic and Message model

--- base/views.py
+++ base/views.py
@@ -4,11 +4,11 @@ from .models import Room
 
 
 # dummy data
-rooms = [
-    {'id': 1, 'name': 'Lets learn python'},
-    {'id': 2, 'name': 'Design with me'},
-    {'id': 3, 'name': 'Frontend with me'},
-]
+# rooms = [
+#     {'id': 1, 'name': 'Lets learn python'},
+#     {'id': 2, 'name': 'Design with me'},
+#     {'id': 3, 'name': 'Frontend with me'},
+# ]
 
 def home(request):
     rooms = Room.objects.all()
@@ -16,9 +16,6 @@ def home(request):
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
