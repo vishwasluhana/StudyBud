f909ba24156b1504e2da4364da832c94e1a48ff3
Vishwa Mittar <vishwasluhana@gmail.com>
2022-09-20 23:37:01 +0500

    add dummy room data

--- base/views.py
+++ base/views.py
@@ -1,8 +1,22 @@
+from multiprocessing import context
 from django.shortcuts import render
 
 
-def home(request):
-    return render(request, 'home.html')
+# dummy data
+rooms = [
+    {'id': 1, 'name': 'Lets learn python'},
+    {'id': 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Frontend with me'},
+]
 
-def room(request):
-    return render(request, 'room.html')
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
