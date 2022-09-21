a021c8da33447c7ef824c41e6850ec42914848cd
Vishwa Mittar <vishwasluhana@gmail.com>
2022-09-21 22:56:23 +0500

    restrict logged in user from login page

--- base/views.py
+++ base/views.py
@@ -11,6 +11,9 @@ from .forms import RoomForm
 
 def loginPage(request):
 
+    if request.user.is_authenticated:
+        return redirect('home')
+
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
