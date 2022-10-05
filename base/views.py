4f385bdd7741afe95a26ef9cecfbd5d785807837
Vishwa Mittar <vishwasluhana@gmail.com>
2022-10-05 21:33:19 +0500

    create view for activityPage

--- base/views.py
+++ base/views.py
@@ -194,3 +194,8 @@ def topicsPage(request):
     q = request.GET.get('q') if request.GET.get('q') is not None else ''
     topics = Topic.objects.filter(name__icontains=q)
     return render(request, 'base/topics.html', {'topics': topics})
+
+
+def activityPage(request):
+    room_meesages = Message.objects.all()
+    return render(request, 'base/activity.html', {'room_messages': room_meesages})
