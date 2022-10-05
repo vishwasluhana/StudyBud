78c85052e7c0e5538f3e8e9dc29519b6041153db
Vishwa Mittar <vishwasluhana@gmail.com>
2022-10-05 20:08:21 +0500

    update UpdateUser for post requests

--- base/views.py
+++ base/views.py
@@ -181,5 +181,10 @@ def updateUser(request):
     user = request.user
     form = UserForm(instance=user)
 
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
 
     return render(request, 'base/update-user.html', {'form': form})
