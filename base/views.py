941ee6f7151c562bcaf61fc0fb8a3e045d898de7
Vishwa Mittar <vishwasluhana@gmail.com>
2022-10-05 21:18:02 +0500

    add topicsPage view

--- base/views.py
+++ base/views.py
@@ -68,7 +68,7 @@ def home(request):
         Q(description__icontains=q)
     )
 
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[:5]
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
@@ -188,3 +188,9 @@ def updateUser(request):
             return redirect('user-profile', pk=user.id)
 
     return render(request, 'base/update-user.html', {'form': form})
+
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') is not None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, 'base/topics.html', {'topics': topics})
