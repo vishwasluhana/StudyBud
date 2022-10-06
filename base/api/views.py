465877731890d50aaf0a1e7bb6a92d7f42190f62
Vishwa Mittar <vishwasluhana@gmail.com>
2022-10-07 01:14:33 +0500

    add rest_framework functions to getRoute view

--- base/api/views.py
+++ base/api/views.py
@@ -1,10 +1,12 @@
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
+@api_view(['GET'])
 def getRoutes(request):
     routes = [
         'GET /api',
         'GET /api/rooms',
         'GET /api/rooms/:id'
     ]
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
