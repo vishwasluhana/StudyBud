50c4a73ccef59c41b5238960a44e17298814eb9f
Vishwa Mittar <vishwasluhana@gmail.com>
2022-10-07 13:28:39 +0500

    create getRoom view

--- base/api/views.py
+++ base/api/views.py
@@ -20,3 +20,10 @@ def getRooms(request):
     rooms = Room.objects.all()
     serializer = RoomSerializer(rooms, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    serializer = RoomSerializer(room, many=False)
+    return Response(serializer.data)
