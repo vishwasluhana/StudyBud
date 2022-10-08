596f6e5a466ec1248176fd584b9526d56d7ac666
Vishwa Mittar <vishwasluhana@gmail.com>
2022-10-08 14:35:48 +0500

    add fields to custom user model

--- base/models.py
+++ base/models.py
@@ -3,7 +3,14 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    name = models.CharField(max_length=100, null=True)
+    email = models.EmailField(null=True, unique=True)
+    bio = models.TextField(null=True)
+
+    avatar = models.ImageField(null=True, default="avatar.svg")
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
 
 class Topic(models.Model):
